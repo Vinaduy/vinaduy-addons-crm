72d54f57a0c0c2a562832a5ce883fa8c2513e5cc
Vinaduy <congtyvinaduy@gmail.com>
2026-06-15 08:38:26 +0700

    feat(elearning): module vd_elearning - thiet ke lai trang tong quan khoa hoc cao cap
    Hero gradient + accent vang kim, restyle card/section, viet hoa tieu de. Ke thua website_slides.courses_home.

---
+++ vd_elearning/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Vinaduy eLearning',
+    'version': '18.0.1.0.0',
+    'category': 'Website/eLearning',
+    'summary': 'Giao dien hoc online cao cap cho Vinaduy',
+    'description': 'Thiet ke lai trang tong quan khoa hoc (courses_home) theo phong cach cao cap, sac net, chuyen nghiep.',
+    'author': 'Vinaduy',
+    'depends': ['website_slides'],
+    'data': [
+        'views/slides_homepage.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'vd_elearning/static/src/scss/elearning.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
